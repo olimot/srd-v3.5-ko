370c4c129463279ce881ca2ac69fec740712fcc6
olimot <olimot@gmail.com>
2021-05-22 21:29:53 +0900

    번역중

--- src/workbench/translate.ts
+++ src/workbench/translate.ts
@@ -1,18 +1,41 @@
 import modifyHtmls from './modifyHtmls';
 import idList from '../../headline-list-id.json';
-import koList from '../../headline-list-ko.json';
+import hko from '../../headline-list-ko.json';
+import hen from '../../headline-list-en.json';
 
 const index = async () => {
   console.log('Translating...');
 
-  const theDictionary = idList.reduce((prev, en, i) => {
-    return { ...prev, [en]: prev[en] || koList[i] };
+  const theHeadlineDict = idList.reduce((prev, en, i) => {
+    return { ...prev, [en]: prev[en] || hko[i] };
   }, {} as Record<string, string>);
+
+  const theDictEntries = hen
+    .reduce((prev, enWord, i) => {
+      const koWord = hko[i];
+      const mainEnWord = (enWord.match(/([^[(]*)/) || [])[0]?.trim() || '';
+      const mainKoWord = (koWord.match(/([^[(]*)/) || [])[0]?.trim() || '';
+      const prevIdx = prev.findIndex(l => l[0] === mainEnWord);
+      if (mainEnWord) {
+        if (prevIdx === -1) return [...prev, [mainEnWord, mainKoWord]];
+        if (prev[prevIdx].indexOf(mainKoWord) === -1)
+          return [...prev.slice(0, prevIdx), [...prev[prevIdx], mainKoWord], ...prev.slice(prevIdx + 1)];
+      }
+      return prev;
+    }, [] as string[][])
+    .sort((a, b) => b[0].length - a[0].length);
+
+  const dictConflicts = theDictEntries.filter(innerlist => innerlist.length !== 2);
+  if (dictConflicts.length) {
+    console.log('The translation has some conflicts:');
+    console.log(dictConflicts);
+    return;
+  }
 
   await modifyHtmls(
     './public/raw-html-en',
     './public/raw-html-ko',
-    id2ko => {
+    ({ theHeadlineDict: id2ko, theDictEntries: dictEntries }) => {
       const [, groupNameEn, pageNameEn] = document.title.split(' / ');
       const groupNameKo = id2ko[` group-name:${groupNameEn}`];
       const pageNameKo = id2ko[` page-name:${pageNameEn}`];
@@ -25,10 +48,31 @@ const index = async () => {
           ? section.querySelector('h1,h2,h3,h4,h5')
           : section) as HTMLElement;
         if (!headline) return;
+        const originalText = headline.textContent;
         headline.textContent = value;
+        const originalTextDiv = document.createElement('div');
+        originalTextDiv.className = 'original-headline';
+        originalTextDiv.textContent = originalText;
+        headline.insertAdjacentElement('afterend', originalTextDiv);
+      });
+      if (pageNameKo === '법률정보') return;
+
+      dictEntries.forEach(([findWord, replaceWord]) => {
+        if (!findWord) return;
+        const findRe = new RegExp(
+          `\\b${findWord.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\s+/g, '\\s+')}(s|es)?\\b`,
+          'gi',
+        );
+        const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+        let node = walker.nextNode();
+        while (node) {
+          if (node.nodeValue && node.parentElement?.className !== 'original-headline')
+            node.nodeValue = node.nodeValue.replace(findRe, replaceWord);
+          node = walker.nextNode();
+        }
       });
     },
-    theDictionary,
+    { theHeadlineDict, theDictEntries },
   );
 };
 
